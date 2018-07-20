e2bcf12661ef4bb859c9231cf5aed4c443cd6146
Timothy Booser <tbooser@utexas.edu>
2018-07-20 10:09:48 -0500

    Pre QA commit, looking close to finished

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 "use strict";
 
 $(document).ready(() => {
-	let current;
+  let active = {}; // Object to keep track of which dropdowns are opened/closed
 
-	$(".offer-box-mobile-details-button").click(event => {
-		current = event.currentTarget.parentElement.className; // Grabs the unique class name from the parent of the click
-		$("#" + current).toggle(); // Toggles the id that matches the class name
-		$(".collapse-" + current).toggle(); // Toggles the collapse button that matches the class name
-		$(".expand-" + current).toggle(); // Toggles the expand button that matches the class name
-	});
+  $(".offer-box-mobile-details-button").click(event => {
+    let current = null; // Stores the class name of the clicked box
+    current = event.currentTarget.parentElement.className; // Grabs the unique class name from the parent of the click
+    if (!active[current] || active[current] === false) {
+      $("#" + current).removeClass("hidden"); // Removes hidden class from the matching text container
+      $("#" + current).addClass("mobile"); // Adds the active class to the matching text container
+      $(".collapse-" + current).toggle(); // Toggles the collapse button that matches the class name
+      $(".expand-" + current).toggle(); // Toggles the expand button that matches the class name
+      active[current] = true; // Sets the current box's active state to true
+    } else if (active[current] === true) {
+      current = event.currentTarget.parentElement.className;
+      $("#" + current).removeClass("mobile");
+      $("#" + current).addClass("hidden");
+      $(".collapse-" + current).toggle();
+      $(".expand-" + current).toggle();
+      active[current] = false;
+    }
+  });
 });
